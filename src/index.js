f524eedc89a0a1f016fa8104496edd7c70c1a6d6
OMAKHMUDOFF <omaxmudovv@gmail.com>
2023-10-25 16:12:44 +0500

    navbar finished, react slick slider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/mainStyle.css";
 import App from "./App";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,48 @@
 import React from "react";
+import Slider from "react-slick";
 
-function Homepage() {
+export default function HomePage() {
+  function SampleNextArrow(props) {
+    const { className, onClick } = props;
+    return <div className={className} onClick={onClick} />;
+  }
+
+  function SamplePrevArrow(props) {
+    const { className, onClick } = props;
+    return <div className={className} onClick={onClick} />;
+  }
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
   return (
-    <div className="HomePage">
-      <h1>HomePage</h1>
+    <div>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
     </div>
   );
 }
-
-export default Homepage;
