390d056aa74f15ef01bd52d62fd4cd2c2325de39
OMAKHMUDOFF <omaxmudovv@gmail.com>
2023-10-25 12:34:48 +0500

    react icons, navbar started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import { HiOutlineBars2 } from "react-icons/hi2";
+import logo from "../assets/img/homePage_IMG/logo 1.png";
 
 function Navbar() {
   return (
@@ -12,27 +14,52 @@ function Navbar() {
                 <NavLink to="/about">О компании</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/payment">Оплата</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/deliver">Доставка</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/refund">Возврат</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/reviews">Отзывы</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/quest-answer">Вопрос-Ответ</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/news">Новости</NavLink>
               </li>
               <li>
-                <NavLink to="/"></NavLink>
+                <NavLink to="/contacts">Контакты</NavLink>
               </li>
             </ul>
+          </div>
+          <div className="top-right-nav">
+            <div className="nav-time">
+              <span>Ежедневно, с 8:00 до 18:00</span>
+            </div>
+            <div className="nav-phone-number">
+              <a href="tel: 88004440065">8 800 444 00 65</a>
+            </div>
+            <div className="nav-order-btn">
+              <button>Заказать звонок</button>
+            </div>
+          </div>
+        </div>
+        <div className="bottom-navbar">
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="nav-catalog-btn">
+            <button>
+              <HiOutlineBars2 />
+              Каталог
+            </button>
+          </div>
+          <div className="nav-search-input">
+            
           </div>
         </div>
       </nav>
